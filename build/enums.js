69644b8eec20e718628690a763c98ccb03b3ade6
morishita <sasaki.adoreillura.sd@gmail.com>
2021-09-15 03:50:32 +0900

    chore: build結果

---
+++ build/enums.js
@@ -0,0 +1,6 @@
+export const defaultProjectName = 'cl-xxxx';
+export var choices;
+(function (choices) {
+    choices["LP"] = "landing";
+    choices["WP"] = "wordpress";
+})(choices || (choices = {}));
--- build/index.js
+++ build/index.js
@@ -1,23 +1,3 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -27,67 +7,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = __importStar(require("fs"));
-var cac_1 = __importDefault(require("cac"));
-var inquirer_1 = require("inquirer");
-var chalk_1 = __importDefault(require("chalk"));
-var cli = (0, cac_1.default)();
-cli.command('[dirName]', '').action(function () { return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        (0, inquirer_1.prompt)([
-            {
-                name: 'projectName',
-                message: 'プロジェクト名を入力してください。',
-                default: 'cl-xxxx',
-            },
-            {
-                type: 'list',
-                name: 'size',
-                message: 'どういった案件ですか？',
-                choices: ['lp', 'wordpress'],
-            },
-        ]).then(function (_a) {
-            var projectName = _a.projectName, size = _a.size;
-            fs.mkdir(projectName, function (error) {
-                if (error) {
-                    console.error(chalk_1.default.red('既に同名のディレクトリが存在します。'));
-                    return;
-                }
-            });
-        });
-        return [2 /*return*/];
+import { existsSync, copySync } from 'fs-extra';
+import { resolve } from 'path';
+import cac from 'cac';
+import { prompt } from 'inquirer';
+import chalk from 'chalk';
+import { prompts } from './prompts';
+import { choices } from './enums';
+const cli = cac();
+cli.command('[dirName]', '').action(() => __awaiter(void 0, void 0, void 0, function* () {
+    const { projectName, size } = yield prompt(prompts).then(({ projectName, size }) => {
+        return { projectName, size };
     });
-}); });
+    const upLevelDir = resolve(__dirname, '..');
+    const targetDir = resolve(upLevelDir, projectName);
+    const templateDir = resolve(upLevelDir, `templates/${size}`);
+    if (existsSync(targetDir)) {
+        console.log(``);
+        console.error(`${chalk.bgRed.black(`Error`)}: 既に同名のディレクトリが存在します。`);
+        console.log(``);
+        return;
+    }
+    if (size === choices.LP) {
+        copySync(templateDir, targetDir);
+        console.log(``);
+        console.log(``);
+        console.log(`🎆${chalk.bgBlack.green(`Success`)}: ${projectName}フォルダの作成に成功しました！🎆`);
+        console.log(``);
+        console.log(`🚀以下のコマンドで実装開始🚀`);
+        console.log(`${chalk.bgBlack.green(1)}: ${chalk.white(`cd ${projectName}`)}`);
+        console.log(`${chalk.bgBlack.green(2)}: ${chalk.white(`pnpm install`)}`);
+        console.log(`${chalk.bgBlack.green(3)}: ${chalk.white(`pnpm run dev`)}`);
+        console.log(``);
+        return;
+    }
+    if (size === choices.WP) {
+        copySync(templateDir, targetDir);
+        return;
+    }
+}));
 cli.help();
 cli.parse();
