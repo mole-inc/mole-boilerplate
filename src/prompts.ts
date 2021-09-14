b138eb98c7e9c399e969f1a1a03f6607de03c1f4
morishita <sasaki.adoreillura.sd@gmail.com>
2021-09-15 03:20:14 +0900

    feat: prompts.ts

---
+++ src/prompts.ts
@@ -0,0 +1,20 @@
+import { defaultProjectName, choices } from './enums'
+
+export type promptsType = {
+  projectName: string
+  size: string
+}
+
+export const prompts = [
+  {
+    name: 'projectName',
+    message: 'プロジェクト名を入力してください。',
+    default: defaultProjectName,
+  },
+  {
+    type: 'list',
+    name: 'size',
+    message: 'どういった案件ですか？',
+    choices: [choices.LP, choices.WP],
+  },
+]
