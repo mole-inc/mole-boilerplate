87267447069f7c64f3435c2e60bcef27eb725dbd
morishita <sasaki.adoreillura.sd@gmail.com>
2021-09-15 20:12:11 +0900

    chore: build結果

--- build/index.js
+++ build/index.js
@@ -49,7 +49,7 @@ var enums_1 = require("./enums");
 var process_1 = require("process");
 var cli = (0, cac_1.default)();
 cli.command('[dirName]', '').action(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, projectName, size, upLevelDir, targetDir, templateDir;
+    var _a, projectName, size, cwdDir, upLevelDir, targetDir, templateDir;
     return __generator(this, function (_b) {
         switch (_b.label) {
             case 0: return [4 /*yield*/, (0, inquirer_1.prompt)(prompts_1.prompts).then(function (_a) {
@@ -58,14 +58,16 @@ cli.command('[dirName]', '').action(function () { return __awaiter(void 0, void 
                 })];
             case 1:
                 _a = _b.sent(), projectName = _a.projectName, size = _a.size;
+                cwdDir = (0, process_1.cwd)();
                 upLevelDir = (0, path_1.resolve)(__dirname, '..');
-                targetDir = (0, path_1.resolve)(upLevelDir, projectName);
+                targetDir = (0, path_1.resolve)(cwdDir, projectName);
                 templateDir = (0, path_1.resolve)(upLevelDir, "templates/" + size);
                 /*
                 debug - target: H:\.pnpm-store\v3\tmp\_npx\27164\5\node_modules\.pnpm\github.com+xaxaxixd+lasso@9d81f05451151e3842845964ab56a1a30ab19a26\node_modules\lasso\cl-xxxx
                 debug - template: H:\.pnpm-store\v3\tmp\_npx\27164\5\node_modules\.pnpm\github.com+xaxaxixd+lasso@9d81f05451151e3842845964ab56a1a30ab19a26\node_modules\lasso\templates\landing
                 */
-                console.log("debug - cwd: " + process_1.cwd);
+                console.log("debug - dirname: " + __dirname);
+                console.log("debug - cwd: " + (0, process_1.cwd)());
                 console.log("debug - target: " + targetDir);
                 console.log("debug - template: " + templateDir);
                 if ((0, fs_extra_1.existsSync)(targetDir)) {
