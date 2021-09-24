692c35e3299d88d31ae05579726dbcd9509f3863
morishita <sasaki.adoreillura.sd@gmail.com>
2021-09-24 09:42:10 +0900

    feat: templates/wordpress - componentsサンプルを追加

--- templates/wordpress/theme/front-page.php
+++ templates/wordpress/theme/front-page.php
@@ -1,7 +1,15 @@
 <?php get_template_part('components/head'); ?>
 
 <main class="Page">
-  <h1>TITLE</h1>
+  <div style="
+    display: grid;
+    place-items: center;
+    width: 100%;
+    height: calc(var(--vh) * 100);
+    background-color: gray;
+  ">
+    <?php get_template_part('components/logo', null, ['fill'=>'#ffffff']); ?>
+  </div>
 </main>
 
 <?php get_template_part('components/foot'); ?>
