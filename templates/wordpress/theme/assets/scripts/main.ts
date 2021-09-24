fafb6631817ac20a6d929ae3fc772068fc3e984a
morishita <sasaki.adoreillura.sd@gmail.com>
2021-09-24 09:41:00 +0900

    fix: templates/wordpress - main.ts内を追記

--- templates/wordpress/theme/assets/scripts/main.ts
+++ templates/wordpress/theme/assets/scripts/main.ts
@@ -1,3 +1,8 @@
 import 'ress'
 
 import '../styles/main.scss'
+
+const root = document.documentElement
+root.style.setProperty('--vh', window.innerHeight / 100 + 'px')
+
+window.addEventListener('load', () => {}, { once: true, passive: true })
