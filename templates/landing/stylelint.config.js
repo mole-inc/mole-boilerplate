1547fb88157c0fc0338ba6152d70b3ec7ed840d9
morishita <40419910+xaxaxixd@users.noreply.github.com>
2022-03-09 15:14:19 +0900

    fix: templates、lint周り削除

--- templates/landing/stylelint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ['stylelint-prettier'],
-  extends: ['stylelint-prettier/recommended', 'stylelint-config-prettier'],
-  ignoreFiles: ['**/node_modules/**'],
-  rules: {
-    'prettier/prettier': true,
-  },
-}
