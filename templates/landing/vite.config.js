0dbe43f07f51c9ed63ec3cb800919aeaa1a3624f
sengokukun <py.kzkmra@gmail.com>
2023-05-24 14:49:50 +0900

    fix: vite.config.js build files

--- templates/landing/vite.config.js
+++ templates/landing/vite.config.js
@@ -1,47 +1,68 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
-import { resolve } from 'path'
+import { resolve, join } from 'path'
 import fs from 'fs'
 
-var fileNames = fs.readdirSync(resolve(__dirname, './src/'))
-var htmlFiles = fileNames.filter(file => /.html$/.test(file))
-var inputFiles = {}
-for (var i = 0; i < htmlFiles.length; i++) {
-  var file = htmlFiles[i]
-  inputFiles[file.slice(0, -5)] = resolve(__dirname, './src/' + file)
+function getAllFiles(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach((file) => {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      if (file !== 'components') {
+        arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
+      }
+    } else {
+      arrayOfFiles.push(join(dirPath, '/', file))
+    }
+  })
+
+  return arrayOfFiles
 }
+
+const htmlFiles = getAllFiles(resolve(__dirname, './src/')).filter((file) =>
+  /.html$/.test(file)
+)
+
+let inputFiles = {}
+
+htmlFiles.forEach((file) => {
+  const pathWithoutSrc = file.split('/src')[1]
+  let fileName = pathWithoutSrc.slice(0, -5)
+  fileName = fileName.replace(/^\//, '')
+  inputFiles[fileName] = file
+})
 
 export default defineConfig({
   target: 'es2015',
-  root: 'src',
+  root: './src',
   build: {
     outDir: '../build',
     rollupOptions: {
       output: {
         chunkFileNames: 'assets/js/[name].js',
         entryFileNames: 'assets/js/[name].js',
-        assetFileNames: (into) => {
-          var extType = into.name.split('.')[1];
+        assetFileNames: (assetInfo) => {
+          const extType = assetInfo.name.split('.')[1]
 
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return 'assets/images/[name].[extname]';
+          if (/png|webp|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return 'assets/images/[name].[ext]'
           }
           if (extType === 'css') {
-            return 'assets/css/[name].css';
+            return 'assets/css/[name].[ext]'
           }
-          return 'assets/[name].[extname]';
+          return 'assets/[name].[ext]'
         },
       },
-      input: inputFiles
+      input: inputFiles,
     },
     emptyOutDir: true,
     manifest: false,
   },
   plugins: [
     handlebars({
-      partialDirectory: [
-        resolve(__dirname, './src/components')
-      ],
+      partialDirectory: [resolve(__dirname, './src/components')],
     }),
   ],
 })
